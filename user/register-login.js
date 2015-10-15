c585e22456c900305871860781a5cbde16d29f47
Teo Paolucci <teo.paolucci@gmail.com>
2015-10-15 12:23:30 -0400

    added functioning close buttons to popups

--- user/register-login.js
+++ user/register-login.js
@@ -1,5 +1,11 @@
+var closeHandler = function closeHandler(){
+  $('#close').on('click', function(){
+    $('.popup').remove();
+  });
+}
+
 $(document).ready(function(){
-  //register button
+  //display register button
   $('#reg-display').on('click', function(){
     $('.userbar').append(
       '<div class="popup">' +
@@ -7,18 +13,22 @@ $(document).ready(function(){
         '<input name="password" type="password" placeholder="Password"><br>' +
         '<input name="password_confirmation" type="password" placeholder="Confirm Password"><br>' +
         '<input name="register" type="submit" value="Register">' +
+        '<input id="close" type="button" value="X" style="background-color: red; float:right;">' +
       '</div>'
     );
+    closeHandler();
   });
 
-  //login button
+  //display login button
   $('#log-display').on('click', function(){
     $('.userbar').append(
       '<div class="popup">' +
         '<input name="email" type="text" placeholder="E-mail"><br>' +
         '<input name="password" type="password" placeholder="Password"><br>' +
         '<input name="login" type="submit" value="Login">' +
+        '<input id="close" type="button" value="X" style="background-color: red; float:right;">' +
       '</div>'
-    )
+    );
+    closeHandler();
   });
 });
